ea3624906905c0b0e12d97bbd42ba97b84d40e5e
orianamachado66 <orianamachado66@gmail.com>
2023-12-16 18:42:36 -0800

    Added the footer

--- src/App.js
+++ src/App.js
@@ -10,6 +10,18 @@ function App() {
 
         <Weather />
       </header>
+      <footer>
+        <div>
+          This project was coded by{" "}
+          <a href="https://www.linkedin.com/in/oriana-machado-771014130/">
+            Oriana Machado
+          </a>{" "}
+          and it is{" "}
+          <a href="https://github.com/orianamachado66/app-react">
+            open sourced
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }
